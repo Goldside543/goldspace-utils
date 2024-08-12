d3c2ca57d754434fca343f299eae850b3395f2a5
Goldside543 <goldside000@outlook.com>
2024-08-11 23:20:56 -0400

    Rewrote GPong

--- gpong/gpong.c
+++ gpong/gpong.c
@@ -10,79 +10,145 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
 
 #define WIDTH 40
 #define HEIGHT 20
 #define PADDLE_HEIGHT 4
-#define PADDLE_WIDTH 1
-#define BALL_SIZE 1
+#define BALL_CHAR 'O'
+#define PADDLE_CHAR '|'
+#define EMPTY_CHAR ' '
 
 typedef struct {
     int x, y;
-} Point;
+} Position;
 
 typedef struct {
-    Point pos;
-    int height, width;
-} Paddle;
-
-typedef struct {
-    Point pos;
-    Point vel;
+    Position pos;
+    Position dir;
 } Ball;
 
-void draw_paddle(Paddle *paddle) {
-    for (int i = 0; i < paddle->height; i++) {
-        printf("\033[%d;%dH#", paddle->pos.y + i, paddle->pos.x);
-    }
-}
-
-void draw_ball(Ball *ball) {
-    printf("\033[%d;%dH*", ball->pos.y, ball->pos.x);
-}
+typedef struct {
+    Position pos;
+    int height;
+} Paddle;
 
 void clear_screen() {
     printf("\033[H\033[J");
 }
 
-void move_paddle(Paddle *paddle, int dy) {
-    paddle->pos.y += dy;
-    if (paddle->pos.y < 1) paddle->pos.y = 1;
-    if (paddle->pos.y + paddle->height > HEIGHT - 1) paddle->pos.y = HEIGHT - 1 - paddle->height;
+void draw_frame(Ball *ball, Paddle *left_paddle, Paddle *right_paddle) {
+    clear_screen();
+    char screen[WIDTH][HEIGHT];
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            screen[x][y] = EMPTY_CHAR;
+        }
+    }
+
+    // Draw paddles
+    for (int i = 0; i < left_paddle->height; i++) {
+        if (left_paddle->pos.y + i < HEIGHT) {
+            screen[left_paddle->pos.x][left_paddle->pos.y + i] = PADDLE_CHAR;
+        }
+    }
+
+    for (int i = 0; i < right_paddle->height; i++) {
+        if (right_paddle->pos.y + i < HEIGHT) {
+            screen[right_paddle->pos.x][right_paddle->pos.y + i] = PADDLE_CHAR;
+        }
+    }
+
+    // Draw ball
+    if (ball->pos.y < HEIGHT && ball->pos.x < WIDTH) {
+        screen[ball->pos.x][ball->pos.y] = BALL_CHAR;
+    }
+
+    // Print screen
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            printf("%c", screen[x][y]);
+        }
+        printf("\n");
+    }
 }
 
-void move_ball(Ball *ball) {
-    ball->pos.x += ball->vel.x;
-    ball->pos.y += ball->vel.y;
-
-    if (ball->pos.y <= 1 || ball->pos.y >= HEIGHT - 1) ball->vel.y = -ball->vel.y;
+int kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+    return 0;
 }
 
-bool check_collision(Ball *ball, Paddle *paddle) {
-    return (ball->pos.x == paddle->pos.x + paddle->width &&
-            ball->pos.y >= paddle->pos.y &&
-            ball->pos.y < paddle->pos.y + paddle->height);
+void update_game(Ball *ball, Paddle *left_paddle, Paddle *right_paddle) {
+    ball->pos.x += ball->dir.x;
+    ball->pos.y += ball->dir.y;
+
+    // Ball collision with top and bottom
+    if (ball->pos.y <= 0 || ball->pos.y >= HEIGHT - 1) {
+        ball->dir.y = -ball->dir.y;
+    }
+
+    // Ball collision with paddles
+    if (ball->pos.x == left_paddle->pos.x + 1 &&
+        ball->pos.y >= left_paddle->pos.y &&
+        ball->pos.y < left_paddle->pos.y + left_paddle->height) {
+        ball->dir.x = -ball->dir.x;
+    }
+
+    if (ball->pos.x == right_paddle->pos.x - 1 &&
+        ball->pos.y >= right_paddle->pos.y &&
+        ball->pos.y < right_paddle->pos.y + right_paddle->height) {
+        ball->dir.x = -ball->dir.x;
+    }
+
+    // Ball out of bounds
+    if (ball->pos.x <= 0 || ball->pos.x >= WIDTH - 1) {
+        ball->pos.x = WIDTH / 2;
+        ball->pos.y = HEIGHT / 2;
+        ball->dir.x = (rand() % 2) ? 1 : -1;
+        ball->dir.y = (rand() % 2) ? 1 : -1;
+    }
+
+    // Move paddles
+    if (kbhit()) {
+        char c = getchar();
+        if (c == 'w' && left_paddle->pos.y > 0) {
+            left_paddle->pos.y--;
+        } else if (c == 's' && left_paddle->pos.y < HEIGHT - left_paddle->height) {
+            left_paddle->pos.y++;
+        } else if (c == 'i' && right_paddle->pos.y > 0) {
+            right_paddle->pos.y--;
+        } else if (c == 'k' && right_paddle->pos.y < HEIGHT - right_paddle->height) {
+            right_paddle->pos.y++;
+        }
+    }
 }
 
 int main() {
-    Paddle left_paddle = {{1, HEIGHT / 2 - PADDLE_HEIGHT / 2}, PADDLE_HEIGHT, PADDLE_WIDTH};
-    Paddle right_paddle = {{WIDTH - 2, HEIGHT / 2 - PADDLE_HEIGHT / 2}, PADDLE_HEIGHT, PADDLE_WIDTH};
     Ball ball = {{WIDTH / 2, HEIGHT / 2}, {1, 1}};
+    Paddle left_paddle = {{2, HEIGHT / 2 - PADDLE_HEIGHT / 2}, PADDLE_HEIGHT};
+    Paddle right_paddle = {{WIDTH - 3, HEIGHT / 2 - PADDLE_HEIGHT / 2}, PADDLE_HEIGHT};
 
-    while (true) {
-        clear_screen();
-        draw_paddle(&left_paddle);
-        draw_paddle(&right_paddle);
-        draw_ball(&ball);
-
-        usleep(100000); // Sleep for 0.1 second
-        move_ball(&ball);
-
-        if (check_collision(&ball, &left_paddle) || check_collision(&ball, &right_paddle)) {
-            ball.vel.x = -ball.vel.x;
-        }
+    while (1) {
+        draw_frame(&ball, &left_paddle, &right_paddle);
+        update_game(&ball, &left_paddle, &right_paddle);
+        usleep(50000); // Sleep for 50 ms
     }
 
     return 0;
