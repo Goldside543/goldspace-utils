9edbd2cdb4cb4fcd0cde3b2431d21ff8f2935cf6
Goldside543 <goldside000@outlook.com>
2024-08-11 21:07:19 -0400

    Update gpong.c

--- gpong/gpong.c
+++ gpong/gpong.c
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * gpong/gpong.c
+ * 
+ * Pong port to Goldspace.
+ *
+ * Copyright (C) Goldside543
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
